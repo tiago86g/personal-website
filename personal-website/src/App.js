a1032ced004a9670a25aa68deddd8896632cd5b6
tiago <tiago86g@gmail.com>
2020-11-11 15:36:22 +0100

    configure React router

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch,Route, Link } from "react-router-dom";
+import AboutOverview from "./Components/AboutOverview/AboutOverview"
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      personal website
+      <Router>
+        {/* <Navbar /> */}
+        <div className="main">
+          <Switch>
+            <Route exact path="/" component={AboutOverview} />
+            {/* <Route exact path="/experience" component={Experience} />
+            <Route exact path="/education" component={Education} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/:id" component={Project} /> */}
+          </Switch>
+        </div>
+
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
---
+++ personal-website/src/Components/AboutOverview/AboutOverview.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AboutOverview() {
+    return (
+        <div>
+            <p>About Overview component</p>
+        </div>
+    )
+}
