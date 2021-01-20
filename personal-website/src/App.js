37d5bbd737cfdb134f8f475853cfc05f07822309
tiago86g <tiago86g@gmail.com>
2021-01-20 21:33:38 +0100

    components streucture and data

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,8 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Switch,Route, Link } from "react-router-dom";
-import AboutOverview from "./Components/AboutOverview/AboutOverview"
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AboutOverview from './components/AboutOverview/AboutOverview';
+import Projects from './components/Projects/Projects';
+import Experience from './components/Experience/Experience';
+import Education from './components/Education/Education';
 
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -12,10 +15,10 @@ function App() {
         <div className="main">
           <Switch>
             <Route exact path="/" component={AboutOverview} />
-            {/* <Route exact path="/experience" component={Experience} />
+            <Route exact path="/experience" component={Experience} />
             <Route exact path="/education" component={Education} />
             <Route exact path="/projects" component={Projects} />
-            <Route exact path="/:id" component={Project} /> */}
+            {/* <Route exact path="/:id" component={Project} /> */}
           </Switch>
         </div>
 
