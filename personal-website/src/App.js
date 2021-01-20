a1fb44c7f103c78e4a3023ca1bc092e2d65531a6
tiago86g <54849930+tiago86g@users.noreply.github.com>
2021-01-20 21:45:33 +0100

    Revert "components streucture and data"

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import AboutOverview from './components/AboutOverview/AboutOverview';
-import Projects from './components/Projects/Projects';
-import Experience from './components/Experience/Experience';
-import Education from './components/Education/Education';
+import React from "react";
+import { BrowserRouter as Router, Switch,Route, Link } from "react-router-dom";
+import AboutOverview from "./Components/AboutOverview/AboutOverview"
 
-import './App.css';
+import './App.css'
 
 function App() {
   return (
@@ -15,10 +12,10 @@ function App() {
         <div className="main">
           <Switch>
             <Route exact path="/" component={AboutOverview} />
-            <Route exact path="/experience" component={Experience} />
+            {/* <Route exact path="/experience" component={Experience} />
             <Route exact path="/education" component={Education} />
             <Route exact path="/projects" component={Projects} />
-            {/* <Route exact path="/:id" component={Project} /> */}
+            <Route exact path="/:id" component={Project} /> */}
           </Switch>
         </div>
 
--- personal-website/src/Components/Education/Education.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Education() {
-  return (
-    <div>
-      <h2>Education component</h2>
-    </div>
-  );
-}
